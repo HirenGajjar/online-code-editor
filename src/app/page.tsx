35635361567b64c38f082035949754062525048a
HirenGajjar <gajjarhiren111@gmail.com>
2024-12-10 18:17:37 -0800

    Add convex backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,26 @@ import {
   SignUpButton,
   SignedIn,
   SignedOut,
+  UserButton,
 } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignUpButton />
+        <SignUpButton>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign up
+          </button>
+        </SignUpButton>
       </SignedOut>
+      <UserButton />
       <SignedIn>
-        <SignOutButton />
+        <SignOutButton>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Sign Out
+          </button>
+        </SignOutButton>
       </SignedIn>
     </div>
   );
